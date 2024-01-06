c2f36e6f3132356df63bf2bfcb8a8fb08be38216
Tess <tess.mueske@gmail.com>
2024-01-06 12:26:28 -0800

    complete lab

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = function(){
+    return 2000/100
+}
+
+const square = (number) => {
+    return number * number
+}
+
+const add = (parameter1, parameter2) => {
+    return parameter1 + parameter2
+}
